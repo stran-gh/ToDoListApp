26ae0092633f311439dc1e593217304eb19d7004
Steve  Tran <stevetran@RESNET-ERC.local>
2017-09-13 20:25:12 -0700

    App Complete

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,9 @@
 import { Chore } from './chore.model';
 
 export class User{
-	constructor(public user: string, public chore: Chore, public choreCount: number){
+	constructor(public user: string, 
+			public chore: Chore, 
+			public choreCount: number){
 
 	}
 }
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,4 +1,8 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { 
+		CanActivate, 
+		ActivatedRouteSnapshot, 
+		RouterStateSnapshot 
+		} from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
